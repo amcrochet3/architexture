f0dda7105da02c7b18fd899109528fc0ad939f79
Allison Crochet <acrochet794@gmail.com>
2023-07-25 02:01:07 -0400

    Completed index

--- server.py
+++ server.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 app.secret_key = os.environ["SECRET_KEY"]
 
 
+
+@app.route('/')
+def index():
+
+    return render_template("index.html")
+
+
+
 if __name__ == "__main__":
     connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
